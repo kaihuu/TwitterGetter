28d41020fa6053f308badb8b3d32cea7cab12d67
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2018-11-14 17:51:01 +0900

    modify

--- TwitterGetter.py
+++ TwitterGetter.py
@@ -1,7 +1,7 @@
 import config, MongoDBAccessor
 import json
 from requests_oauthlib import OAuth1Session, OAuth1
-import requests, sys
+import requests, sys, time
 from datetime import datetime
 
 ck = config.consumer_key
@@ -21,7 +21,7 @@ res = twitter.get(url, params = params)
 
 results = json.loads(res.text)
 
-
+nRstTm = 15 # Rate Limited待機時間
 
 #for result in results['statuses']:
     #print(result)
@@ -39,12 +39,23 @@ auth = OAuth1(ck, cs, at, ats)
 
 while(True):
     try:
+        print('リクエスト')
         r = requests.post(url2, auth=auth, stream=True, data={"track":"学生フォーミュラ, #学生フォーミュラ"})
         if r.status_code == 200:
+            print('コード')
+            print(r)
             for line in r.iter_lines():
                 print(json.loads(line.decode('utf-8')))
                 print()
                 MongoDBAccessor.InsertJSON(json.loads(line.decode('utf-8')))
+        # 短時間でアクセスしすぎたら420エラーになるので待機
+        elif r.status_code == 420:
+            print('Rate Limited：',nRstTm,'分待機')
+            time.sleep(nRstTm)
+        else:
+            # とりあえず処理抜けとく
+            print('req.status_code',r.status_code)
+            break
     
     except json.JSONDecodeError as e:
         print('再取得')
